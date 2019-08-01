6edcdb6d84d35f3c4aee454f2a27a0f80e52ae1e
manbuxingkong <manbuxingkong@aliyun.com>
2019-08-01 17:07:36 +0800

    Initial commit

---
+++ app/src/main/java/com/mbxk/customindexbar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mbxk.customindexbar;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
